f180e0f75072ec9f8887c6c217d3d9abfcc2817d
Iamanaws <Angel.Antonio.Juarez.G@gmail.com>
2022-06-25 00:43:35 +0000

    Lightning Web Component Finished

---
+++ Salesforce Developer Bedu/force-app/main/default/lwc/searchProduct/searchProduct.js
@@ -0,0 +1,100 @@
+import { LightningElement, api } from 'lwc';
+import search from '@salesforce/apex/LightningController.search'
+import save from '@salesforce/apex/LightningController.save'
+
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+
+export default class SearchProduct extends LightningElement {
+    
+    prod;
+    priceBookEntry;    
+    productName;
+    productCode;
+    unitPrice;
+    disponible;
+
+    prodCode = '';
+    ReservarCantidad = '';
+    @api recordId;
+
+    showDetails = false;
+    
+    searchValue(event) {
+        this.prodCode = event.target.value;
+    }
+    saveValue(event) {
+        this.ReservarCantidad = event.target.value;
+    }
+
+    searchProduct() {
+        if(this.prodCode !== '') {
+            search({
+                prodCode: this.prodCode
+            })
+            .then(result => {
+                this.prod = result;
+                this.priceBookEntry = result.PricebookEntries[0];
+                this.inventario = result.Inventarios__r[0];
+
+                this.productName = result.Name;
+                this.productCode = result.ProductCode;
+                this.unitPrice = result.PricebookEntries[0].UnitPrice;
+                this.disponible = result.Inventarios__r[0].Cantidad_dis__c;
+
+                this.showDetails = true;
+            })
+            .catch(error => {
+                this.dispatchEvent(new ShowToastEvent({
+                    variant:'error',
+                    message: error.body.message
+                }));
+
+                this.showDetails = false;
+            })
+        }
+        else {
+            this.dispatchEvent(new ShowToastEvent({
+                variant: 'error',
+                message: 'No se a ingresado un codigo de Producto'
+                }));
+            
+            this.showDetails = false;
+        }
+    }
+
+    saveProduct() {
+        if(this.ReservarCantidad !== '') {
+            save({
+                QuoteId: this.recordId,
+                PricebookEntryId: this.priceBookEntry.Id,
+                Product2Id: this.prod.Id,
+                ReservarCantidad: this.ReservarCantidad,
+                UnitPrice: this.unitPrice
+            })
+            .then(result => {
+                eval("$A.get('e.force:refreshView').fire();");
+                this.dispatchEvent(new ShowToastEvent({
+                    variant:'success',
+                    message: 'El producto se agrego correctamente.'
+                })); 
+            })
+            .catch(error => {
+                this.dispatchEvent(new ShowToastEvent({
+                    variant:'error',
+                    message: error.body.message
+                }));                
+            })
+        }
+        else {
+            this.dispatchEvent(new ShowToastEvent({
+                variant: 'error',
+                message: 'No se ha indicado la cantidad a reservar.'
+                }));
+        }
+    }
+
+    previous() {
+        this.prodCode = '';
+        this.showDetails = false;
+    }
+}
